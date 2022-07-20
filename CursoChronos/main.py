de388a37d4598311a47e9b7ee062c47cfdb5e12a
andersonanunes <andersonanunes@hotmail.com>
2022-07-20 08:27:27 -0300

    Curso Chronos

---
+++ CursoChronos/main.py
@@ -0,0 +1,7 @@
+# interpolação de parametros
+def apresentacao(nome, escola):
+    print(f'Olá, eu sou {nome} aluno da {escola}')
+    print('Estou me desafiando a aprender Python, vamos juntos!')
+
+if __name__ == '__main__':
+    apresentacao('Anderson', 'Chronos Academy')
